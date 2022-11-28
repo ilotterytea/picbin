17c22c0b2eb8ff4771886f207436b2bab86dc456
NotDankEnough <hatesociety@proton.me>
2022-11-29 01:03:50 +0600

    removed dank lines

--- routes/retrieve.py
+++ routes/retrieve.py
@@ -16,47 +16,17 @@ from os.path import exists
 from flask import send_file
 
 from .RoutesBlueprint import blueprint
-from utils.configuration import Configuration
-from utils.json_manipulator import JSONManipulator
-
 
 @blueprint.get("/<id>")
-def retrieve(id: str):
+async def retrieve(id: str):
     """GET route for retrieving the file"""
 
     # Return 404 if ID not exists
-    if id not in JSONManipulator.content:
+    if not exists(f"uploads/{id}"):
         return {
             "file": None,
             "status": 404,
             "message": "Image with ID {} has never existed or its lifetime has expired!".format(id)
         }, 404
 
-
-    # Local file data:
-    DATA = JSONManipulator.content[id]
-
-    # Path based on hash:
-    HASH_BASED_PATH = "{}/{}{}".format(
-                Configuration.content.get("SERVER", "LOCAL_FOLDER"),
-                DATA["hash"],
-                DATA["ext"]
-    )
-
-    # Path based on random hex:
-    HEX_BASED_PATH = "{}/{}{}".format(
-                Configuration.content.get("SERVER", "LOCAL_FOLDER"),
-                DATA["id"],
-                DATA["ext"]
-    )
-
-    if exists(HASH_BASED_PATH):
-        return send_file(HASH_BASED_PATH)
-    elif exists(HEX_BASED_PATH):
-        return send_file(HEX_BASED_PATH)
-    else:
-        return {
-            "file": None,
-            "status": 404,
-            "message": "Image with ID {} has never existed or its lifetime has expired!".format(id)
-         }, 404
+    return send_file(f"uploads/{id}")
