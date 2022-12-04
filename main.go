0fd68efe5c6b97108f10fead743d218dbe3f04ad
NotDankEnough <hatesociety@proton.me>
2022-12-04 15:51:37 +0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, my beloved world!")
+	})
+
+	log.Fatal(app.Listen(":5001"))
+}
