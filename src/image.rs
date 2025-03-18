116c8f17c4e149a4ff00e11601a6ab8080f09bbb
ilotterytea <me@ilotterytea.kz>
2025-03-18 20:40:04 +0500

    feat: save image data in database

--- src/image.rs
+++ src/image.rs
@@ -2,11 +2,16 @@ use std::fs::{copy, create_dir_all, exists};
 
 use actix_multipart::form::{MultipartForm, tempfile::TempFile};
 use actix_web::HttpResponse;
-use serde::Serialize;
+use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl, SelectableHelper};
 
 use crate::{
     Response,
     config::DEFAULT_MIME_TYPES,
+    database::{
+        establish_connection,
+        models::{Image, NewImage},
+        schema::images::dsl as im,
+    },
     random::{self, generate_random_sequence},
 };
 
@@ -15,19 +20,12 @@ pub struct ImageUpload {
     pub file: TempFile,
 }
 
-#[derive(Serialize)]
-struct ImageData {
-    pub id: String,
-    pub mime: String,
-    pub extension: String,
-}
-
 pub async fn handle_image_upload(MultipartForm(form): MultipartForm<ImageUpload>) -> HttpResponse {
     if !exists("userdata").unwrap_or(false) && create_dir_all("userdata").is_err() {
         return HttpResponse::InternalServerError().json(Response {
             status_code: 500,
             message: Some("Failed to create a directory for user data".into()),
-            data: None::<ImageData>,
+            data: None::<Image>,
         });
     }
 
@@ -37,7 +35,7 @@ pub async fn handle_image_upload(MultipartForm(form): MultipartForm<ImageUpload>
             return HttpResponse::BadRequest().json(Response {
                 status_code: 400,
                 message: Some("Unknown MIME type".into()),
-                data: None::<ImageData>,
+                data: None::<Image>,
             });
         }
     };
@@ -51,7 +49,7 @@ pub async fn handle_image_upload(MultipartForm(form): MultipartForm<ImageUpload>
             return HttpResponse::BadRequest().json(Response {
                 status_code: 400,
                 message: Some(format!("Unsupported MIME type: {}", mime.essence_str())),
-                data: None::<ImageData>,
+                data: None::<Image>,
             });
         }
     };
@@ -65,19 +63,32 @@ pub async fn handle_image_upload(MultipartForm(form): MultipartForm<ImageUpload>
         return HttpResponse::InternalServerError().json(Response {
             status_code: 500,
             message: Some("Failed to copy the file".into()),
-            data: None::<ImageData>,
+            data: None::<Image>,
         });
     }
 
-    let image_data = ImageData {
-        id,
-        mime: mime.essence_str().to_string(),
-        extension: extension.to_string(),
+    let filename = match form.file.file_name {
+        Some(n) => n,
+        None => format!("{}.{}", id, extension),
     };
+
+    // saving image data
+    let conn = &mut establish_connection();
+
+    let image = diesel::insert_into(im::images)
+        .values(NewImage {
+            id: &id,
+            filename: &filename,
+            extension,
+            mime: mime.essence_str(),
+        })
+        .returning(Image::as_returning())
+        .get_result(conn)
+        .expect("Error saving new image");
 
     HttpResponse::Created().json(Response {
         status_code: 201,
         message: None,
-        data: Some(image_data),
+        data: Some(image),
     })
 }
